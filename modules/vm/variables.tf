d81a77b16dd71d9f5fb49d2bea5ddc504d945ad1
Bhavika Chauhan <137499857+BhavikaChauhan@users.noreply.github.com>
2025-09-15 23:41:29 +0530

    Create variables.tf

---
+++ modules/vm/variables.tf
@@ -0,0 +1,15 @@
+variable "rg_name" {
+  type = string
+}
+variable "rg_location" {
+  type = string
+}
+variable "subnet_ids" {
+  type = list(string)
+}
+variable "admin_username" {
+  type = string
+}
+variable "admin_password" {
+  type = string
+}
